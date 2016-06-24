5114e587b24e953036e10084aea64bb4a0f93ca7
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-06-24 11:35:46 -0400

    Added New Strategy

--- Othellopt4.py
+++ Othellopt4.py
@@ -7,30 +7,38 @@ def showboard(string):
         print(str(int((i/16)))+str("|")+""+string[i:i+16]+"|"+str(int((i/16))))
     print(" ------------------")
     print("  0 1 2 3 4 5 6 7")
+
+
 def getequivalency():
     pos={}
     for i in range(8):
         pos[i] = {}
         for x in range(8):
-            pos[i][x]=x
-    flipx = {}
-    flip1x = {}
+            pos[i][x]=(i*8)+x
+    flipd = {}
+    flipo = {}
     RR = {}
     R2 = {}
     RL = {}
+    flipx = {}
+    flipy={}
     for i in range(8):
-        flip1x[i] ={}
-        flipx[i] = {}
+        flipo[i] ={}
+        flipd[i] = {}
         RR[i] = {}
         R2[i] = {}
         RL[i] = {}
+        flipx[i] = {}
+        flipy[i]= {}
         for x in range(8):
-            flipx[i][x]=pos[7-x][7-i]
-            flip1x[i][x] = pos[x][i]
+            flipd[i][x]=pos[7-x][7-i]
+            flipo[i][x] = pos[x][i]
             RR[i][x] = pos[x][7-i]
             R2[i][x] = pos[7-i][7-x]
             RL[i][x]=pos[7-x][7-(7-i)]
-    return {"RR":RR,"R2":R2,"RL":RL,"FX":flipx,"F1X":flip1x}
+            flipx[i][x] = pos[i][7-x]
+            flipy[i][x] = pos[7-i][x]
+    return {"RL":RR,"R2":R2,"RR":RL,"FD":flipd,"FO":flipo, "FY":flipx,"FX":flipy, "I":pos}
 
 def getneighbors():
     neighbors = {}
@@ -47,6 +55,8 @@ def getneighbors():
         neighbors[pos]=neighbors[pos]-set(range(64,100))-set(range(-10,0))
         if pos%8==7:
             neighbors[pos] = neighbors[pos]-set(range(0,64,8))
+        elif pos%8==0:
+            neighbors[pos] = neighbors[pos]-set(range(7,64,8))
     return neighbors
 
 def posmoves(string, char):
@@ -79,6 +89,7 @@ def posmoves(string, char):
                 dictposmoves.add(Temppos)
     print(str(dictposmoves))
     return dictposmoves
+
 def playmove(string, char, pos, neighhors):
     posdirections ={}
     flip = set([pos])
@@ -123,8 +134,10 @@ def transform(string, operation):
         for x in range(8):
             Temp+=string[operation[i][x]]
     return Temp
+
 def playgame(string, play):
     neighhors = getneighbors()
+    print(str(neighhors))
     currentgame = string
     players = ["O","X"]
     currplayer = 1
@@ -139,39 +152,48 @@ def playgame(string, play):
             print(players[currplayer]+"'s turn\tRow Col")
             if play[1-currplayer]=="p":
                 Temp = input()
-                if Temp in equivalnce:
+                if Temp.upper() in equivalnce:
                     newstr = string
-                    while not Temp=="PL":
-                        newstr = transform(newstr,equivalnce[Temp])
+                    if Temp.upper == "I":
+                        newstr = transform(string,equivalnce[Temp.upper()])
                         showboard(newstr)
                         Temp = input()
-                Temp = Temp.split()
-                pos = -1
-                if len(Temp)==1 and len(Temp[0])==1 and int(Temp[0])<7:
-                    pos = Temp[0]
-                    print(str(pos))
-                elif "," in Temp[0]:
-                    row = int(Temp[0][0])
-                    if len(Temp)>1:
-                        col = int(Temp[1])
                     else:
-                        col = int(Temp[0][2])
-                elif int(Temp[0])>7:
-                    pos = int(Temp[0])
+                        while not Temp.upper()=="I":
+                            #print(equivalnce[Temp.upper()])
+                            newstr = transform(string,equivalnce[Temp.upper()])
+                            showboard(newstr)
+                            Temp = input()
                 else:
-                    row = int(Temp[0])
-                    col = int(Temp[1])
-                if pos==-1:
-                    pos = (row*8)+col
-                #print(str(pos
-                print(str(pos))
-                print(str(posmov))
-                if pos in posmov:
-                    #print(string)
-                    string = playmove(string, players[currplayer], pos, neighhors)
-                    currplayer = 1-currplayer
-                else:
-                    print("Not a valid move")
+                    Temp = Temp.split()
+                    pos = -1
+                    if len(Temp)==1 and len(Temp[0])==1 and int(Temp[0])<8:
+                        pos = int(Temp[0])
+                        #print(str(pos))
+                    elif "," in Temp[0]:
+                        row = int(Temp[0][0])
+                        if len(Temp)>1:
+                            col = int(Temp[1])
+                        else:
+                            col = int(Temp[0][2])
+                    elif int(Temp[0])>7:
+                        print(str(pos))
+                        pos = int(Temp[0])
+
+                    else:
+                        row = int(Temp[0])
+                        col = int(Temp[1])
+                    if pos==-1:
+                        pos = (row*8)+col
+                    #print(str(pos
+                    #print(str(pos))
+                    #print(str(posmov))
+                    if pos in posmov:
+                        print(posmov)
+                        string = playmove(string, players[currplayer], pos, neighhors)
+                        currplayer = 1-currplayer
+                    else:
+                        print("Not a valid move")
             else:
                 pos = random.choice(list(posmov))
                 print(str(pos))
